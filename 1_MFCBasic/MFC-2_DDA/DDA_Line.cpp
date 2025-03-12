6b5a67b9a5d3ebe4711a164acdadf07f6e0636a5
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-12 15:01:49 +0800

    fix: line draw error when k < 0

--- 1_MFCBasic/MFC-2_DDA/DDA_Line.cpp
+++ 1_MFCBasic/MFC-2_DDA/DDA_Line.cpp
@@ -1,9 +1,9 @@
 #include "pch.h"
 #include "DDA_Line.h"
 
-DDA_Line::DDA_Line(CDC* pDC)
+DDA_Line::DDA_Line(CDC* pDC, int step): step(step), pDC(pDC)
 {
-	this->pDC = pDC;
+
 }
 
 DDA_Line::~DDA_Line()
@@ -13,9 +13,9 @@ DDA_Line::~DDA_Line()
 
 void DDA_Line::DrawLine(CPoint start, CPoint end, COLORREF color)
 {
-	const double K = end.x - start.x ? fabs((end.y - start.y) / (end.x - start.x)) : -1;
+	const double K = end.x - start.x ? 1.0 * (end.y - start.y) / (end.x - start.x) : NAN;
 
-	if (0 <= K && K <= 1)
+	if (!isnan(K) && fabs(K) <= 1)
 	{
 		if (start.x > end.x)
 		{
@@ -24,8 +24,12 @@ void DDA_Line::DrawLine(CPoint start, CPoint end, COLORREF color)
 			end.x = start.x ^ end.x;
 			start.x = start.x ^ end.x;
 		}
-		auto next_point = [](CPoint* p, int k) {p->Offset(1, k); return *p;};
-		while (start.x - end.x) this->pDC->SetPixel(next_point(&start, K), color);		
+		auto next_point = [this](CPoint* p, double k)
+		{
+			p->Offset(this->step, round(k));
+			return *p;
+		};
+		while (start.x - end.x <= 0) this->pDC->SetPixel(next_point(&start, K), color);		
 	} else
 	{
 		if (start.y > end.y)
@@ -35,7 +39,11 @@ void DDA_Line::DrawLine(CPoint start, CPoint end, COLORREF color)
 			end.y = start.y ^ end.y;
 			start.y = start.y ^ end.y;
 		}
-		auto next_point = [](CPoint* p, int k) {p->Offset(k, 1); return *p;};
-		while (start.y - end.y) this->pDC->SetPixel(next_point(&start, K < 0 ? 0 : K), color);
+		auto next_point = [this](CPoint* p, double k)
+		{
+			p->Offset(round(k), this->step);
+			return *p;
+		};
+		while (start.y - end.y <= 0) this->pDC->SetPixel(next_point(&start, isnan(K) ? 0 : K), color);
 	}
 }
