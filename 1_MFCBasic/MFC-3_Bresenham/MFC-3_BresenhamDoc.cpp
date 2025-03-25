646f8cbfb5aeb846f9fd14a31144937f565ed918
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-25 10:04:54 +0800

    feat: add MFC-3_Bresenham

---
+++ 1_MFCBasic/MFC-3_Bresenham/MFC-3_BresenhamDoc.cpp
@@ -0,0 +1,138 @@
+﻿
+// MFC-3_BresenhamDoc.cpp: CMFC3BresenhamDoc 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC-3_Bresenham.h"
+#endif
+
+#include "MFC-3_BresenhamDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMFC3BresenhamDoc
+
+IMPLEMENT_DYNCREATE(CMFC3BresenhamDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CMFC3BresenhamDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CMFC3BresenhamDoc 构造/析构
+
+CMFC3BresenhamDoc::CMFC3BresenhamDoc() noexcept
+{
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CMFC3BresenhamDoc::~CMFC3BresenhamDoc()
+{
+}
+
+BOOL CMFC3BresenhamDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// CMFC3BresenhamDoc 序列化
+
+void CMFC3BresenhamDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CMFC3BresenhamDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CMFC3BresenhamDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:     strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CMFC3BresenhamDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CMFC3BresenhamDoc 诊断
+
+#ifdef _DEBUG
+void CMFC3BresenhamDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CMFC3BresenhamDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CMFC3BresenhamDoc 命令
