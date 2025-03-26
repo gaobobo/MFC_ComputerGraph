166deeddf47e74bc03a3e55abd0deab82f4720d1
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-26 18:00:39 +0800

    refactor(MFC-3_Bresenham)!: change members to private

--- 1_MFCBasic/MFC-3_Bresenham/Bresenham.h
+++ 1_MFCBasic/MFC-3_Bresenham/Bresenham.h
@@ -3,14 +3,15 @@
 class Bresenham
 {
 public:
+    Bresenham(CPoint center, int r, CDC* pDC);
+    
+    void Draw(COLORREF color = RGB(0, 0, 0));
+
+private:
     int x = 0;
     int y = 0;
     int r = 0;
     CPoint center = CPoint(0, 0);
     double d = 1.25;
     CDC* pDC;
-
-    Bresenham(CPoint center, int r, CDC* pDC);
-    
-    void Draw(COLORREF color = RGB(0, 0, 0));
 };
