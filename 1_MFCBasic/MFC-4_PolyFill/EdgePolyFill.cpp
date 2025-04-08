eee74acd53e02e8ae873e095b8193462ea232426
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-04-08 19:16:32 +0800

    chore: fix comment error from others project by accident

--- 1_MFCBasic/MFC-4_PolyFill/EdgePolyFill.cpp
+++ 1_MFCBasic/MFC-4_PolyFill/EdgePolyFill.cpp
@@ -1,6 +1,4 @@
-﻿// MFC-3_WuView.cpp: CMFC3WuView 类的实现
-//
-/*
+﻿/*
 * 对于最新版本(2018及以上)，使用 #include "pch.h" 而非 #include "stdafx.h"
  * 或通过“解决方案管理器 -> 右键项目 -> 属性-> C/C++ -> 预编译头”中查看预编译头
  * 名称。如果你是从Gitee/Github下载的该项目，那么该名称已经修改为 stdafx.h 。
