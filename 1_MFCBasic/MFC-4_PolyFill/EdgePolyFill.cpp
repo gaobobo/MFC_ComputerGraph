e0c3a5784f1380a53fea58894a2e3a7b76f6a22f
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-04-08 19:04:25 +0800

    fix: error when K=0

--- 1_MFCBasic/MFC-4_PolyFill/EdgePolyFill.cpp
+++ 1_MFCBasic/MFC-4_PolyFill/EdgePolyFill.cpp
@@ -35,7 +35,7 @@ void EdgePolyFill::Draw(CPoint* poly, int point_num, int step)
     {
         CPoint p_max = poly[i].y > poly[i - 1].y ? poly[i] : poly[i - 1];
         CPoint p_min =  poly[i].y < poly[i - 1].y ? poly[i] : poly[i - 1];
-        double K = 1.0 * (p_min.x - p_max.x) / (p_min.y - p_max.y);
+        double K = p_min.y == p_max.y ? 1.0 * (p_min.x - p_max.x) / (p_min.y - p_max.y) : 0;
         double x_actual = p_min.x;
         
         for (int y = p_min.y; y < p_max.y; y++)
