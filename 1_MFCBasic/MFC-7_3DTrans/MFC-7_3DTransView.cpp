8ff314ccc16422d4f8782d9e2607c96ee2ed776e
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-05-18 13:30:10 +0800

    feat(MFC-7): init project

---
+++ 1_MFCBasic/MFC-7_3DTrans/MFC-7_3DTransView.cpp
@@ -0,0 +1,105 @@
+﻿
+// MFC-7_3DTransView.cpp: CMFC73DTransView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC-7_3DTrans.h"
+#endif
+
+#include "MFC-7_3DTransDoc.h"
+#include "MFC-7_3DTransView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC73DTransView
+
+IMPLEMENT_DYNCREATE(CMFC73DTransView, CView)
+
+BEGIN_MESSAGE_MAP(CMFC73DTransView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CMFC73DTransView 构造/析构
+
+CMFC73DTransView::CMFC73DTransView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC73DTransView::~CMFC73DTransView()
+{
+}
+
+BOOL CMFC73DTransView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC73DTransView 绘图
+
+void CMFC73DTransView::OnDraw(CDC* /*pDC*/)
+{
+	CMFC73DTransDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFC73DTransView 打印
+
+BOOL CMFC73DTransView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFC73DTransView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMFC73DTransView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CMFC73DTransView 诊断
+
+#ifdef _DEBUG
+void CMFC73DTransView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC73DTransView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC73DTransDoc* CMFC73DTransView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC73DTransDoc)));
+	return (CMFC73DTransDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC73DTransView 消息处理程序
---
+++ 1_MFCBasic/MFC-7_3DTrans/MFC-7_3DTransView.h
@@ -0,0 +1,49 @@
+﻿
+// MFC-7_3DTransView.h: CMFC73DTransView 类的接口
+//
+
+#pragma once
+
+
+class CMFC73DTransView : public CView
+{
+protected: // 仅从序列化创建
+	CMFC73DTransView() noexcept;
+	DECLARE_DYNCREATE(CMFC73DTransView)
+
+// 特性
+public:
+	CMFC73DTransDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMFC73DTransView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFC-7_3DTransView.cpp 中的调试版本
+inline CMFC73DTransDoc* CMFC73DTransView::GetDocument() const
+   { return reinterpret_cast<CMFC73DTransDoc*>(m_pDocument); }
+#endif
+
